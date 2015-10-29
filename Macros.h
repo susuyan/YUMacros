5638480962b049188c7692ddfec1c5274d649a47
susuyan <susuyan@163.com>
2015-10-29 10:34:51 +0800

    debug

--- Macros.h
+++ Macros.h
@@ -178,4 +178,41 @@ static inline void TimeThisBlock (void (^block)(void), NSString *message) {
     LOG(@"Took %f seconds to %@", (CGFloat)nanos / NSEC_PER_SEC, message);
 }
 
+#pragma mark -
+#pragma mark Debug
+#ifdef DEBUG
+#define NSLog(args...) ConciseNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,args);
+#else
+#define NSLog(x...)
+#endif
+
+void ConciseNSLog(const char *file, int lineNumber, const char *functionName, NSString *format, ...);
+
+void ConciseNSLogWithoutTimeStamp(const char *file, int lineNumber, const char *functionName, NSString *format, ...);
+
+// NSLogX(...) -> ExtendNSLog(...)
+// Define NSLogX(), include function name, file name and line number
+#define NSLogX(args...) ExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,args);
+
+void ExtendNSLog(const char *file, int lineNumber, const char *functionName, NSString *format, ...);
+
+// Log class name and method name
+#define LogMethod NSLog(@"-[%@ %@]", NSStringFromClass(self.class),NSStringFromSelector(_cmd))
+
+// NSInteger to NSString
+#define NSIntegerToString(i) [NSString stringWithFormat:@"%lu", (unsigned long)i]
+
+// Print an Object, to see if this object is nil
+#define PrintNil(object) NSLog(@"%@ is %@", [object class], object == nil? @"nil" : @"not nil")
+
+// Detect 4inch or 3.5inch
+#define IS_IPHONE_5 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
+
+// Swap two numbers
+#define OVERLOADABLE __attribute__((overloadable))
+OVERLOADABLE void Swap(float *a, float *b);
+OVERLOADABLE void Swap(NSInteger *a, NSInteger *b);
+OVERLOADABLE void Swap(NSUInteger *a, NSUInteger *b);
+OVERLOADABLE void Swap(double *a, double *b);
+
 #endif
